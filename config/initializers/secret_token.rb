39ed347ed42b604453fb939ca41b3446aa304688
root <alevellop@gmail.com>
2014-09-18 16:23:56 +0200

    Create model Post

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NewBlog::Application.config.secret_key_base = '9621af9ed4e063a80e1360bfc7b25c31040864f4773fda5c84737560392ea313adf7259db7ee7376d48a2911559eea93f5014783d2dbab18a59adb35ba1f8356'
